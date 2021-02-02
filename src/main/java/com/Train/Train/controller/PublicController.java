3e78869a8bd28943d786c356f8c9e4b893884f32
abhilash <abhilash@nexthoughts.com>
2021-02-02 09:11:31 +0530

    first commit

---
+++ src/main/java/com/Train/Train/controller/PublicController.java
@@ -0,0 +1,14 @@
+package com.Train.Train.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+     
+	@RequestMapping("/sayHello")
+	public String sayHello() {
+		return "Hello World";
+	}
+}
